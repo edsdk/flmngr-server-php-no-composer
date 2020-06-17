b05d229ede8932fc1507293f0fd64c05c8651d5c
dmitriy-komarov <dmitriy-komarov@3627127.no-reply.drupal.org>
2020-06-17 14:37:37 +0300

    Updated

--- dist.sh
+++ dist.sh
@@ -25,23 +25,5 @@ cd ..
 
 # ZIP
 
-zip flmngr.zip -r flmngr
+zip flmngr-php.zip -r flmngr
 
-
-
-# D8 edition
-
-mkdir flmngr-d8
-cd flmngr-d8
-cp -rp ../../vendor .
-
-cp -rp ../../flmngr-d8.php flmngr.php
-cd ..
-
-
-# TODO: J3
-
-# add this right after "namespace xxxx;"
-#
-#// No direct access to this file
-#defined('_JEXEC') or die('Restricted access');
